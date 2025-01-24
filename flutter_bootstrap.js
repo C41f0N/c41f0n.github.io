b2e0353492f6a1c6e2853e8eb4b2e9075cd66bcb
Sarim Ahmed <C41f0N@protonmail.com>
2025-01-24 15:47:46 +0500

    Redeploying

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3289098322"
+    serviceWorkerVersion: "2501318813"
   }
 });
